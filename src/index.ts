2350fa687c9b10b2b38122f9d534235b8f85a62d
Aiden <amb@aspy.dev>
2024-05-07 14:43:46 -0400

    Package Updates

--- src/index.ts
+++ src/index.ts
@@ -26,26 +26,30 @@ import { handleCommand } from "./handler/command.js";
 import { handleButton } from "./handler/button.js";
 import { handleAutocomplete } from "./handler/autocomplete.js";
 
-client.on(Events.InteractionCreate, async (interaction) => {
-    if (interaction.isCommand()) {
-        await handleCommand(
-            interaction as ChatInputCommandInteraction,
-            commands,
-        );
-    } else if (interaction.isButton()) {
-        await handleButton(interaction, commands);
-    } else if (interaction.isAutocomplete()) {
-        await handleAutocomplete(interaction, commands);
-    }
+client.on(Events.InteractionCreate, (interaction) => {
+    (async ()=> {
+        if (interaction.isCommand()) {
+            await handleCommand(
+                interaction as ChatInputCommandInteraction,
+                commands,
+            );
+        } else if (interaction.isButton()) {
+            await handleButton(interaction, commands);
+        } else if (interaction.isAutocomplete()) {
+            await handleAutocomplete(interaction, commands);
+        }
+    })
 });
 
 client.on(Events.Error, (error) => {console.error(error)});
 client.on(Events.Warn, (warning) => {console.warn(warning)});
-client.on(Events.Invalidated, async () => {
-    console.log("Session Invalidated - Stopping Client");
-    await client.destroy();
-    await destroy();
-    process.exit(1);
+client.on(Events.Invalidated, () => {
+    (async ()=>{
+        console.log("Session Invalidated - Stopping Client");
+        await client.destroy();
+        await destroy();
+        process.exit(1);
+    });
 });
 
 await client.login(DiscordToken);
